3306266538ab8c16e8444c9b27782b4da8ae9fe1
Mészáros Nándor <farkas.nandor.meszaros@gmail.com>
2019-02-17 14:28:13 +0100

    Initial.

---
+++ src/main/java/org/diamond/dog/DogMod.java
@@ -0,0 +1,31 @@
+package org.diamond.dog;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = DogMod.MODID, name = DogMod.NAME, version = DogMod.VERSION)
+public class DogMod
+{
+    public static final String MODID = "org.diamond.dog";
+    public static final String NAME = "Dog Mod";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
